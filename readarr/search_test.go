c689a6862eaf69f7bcc5f62e22e0e120529dd20d
inku0 <ingvar@saarend.com>
2025-12-03 19:57:43 +0200

    created mock data

--- readarr/search_test.go
+++ readarr/search_test.go
@@ -15,76 +15,69 @@ import (
 )
 
 var testSearchJSON = `{
-		"foreignId": "3634639",
+		"foreignId": "123456789",
 		"book": {
-			"title": "Dune",
-			"authorTitle": "herbert, frank Dune",
+			"title": "Book",
+			"authorTitle": "lastname, firstname Book",
 			"seriesTitle": "",
 			"disambiguation": "",
-			"overview": "Set on the desert planet Arrakis, Dune is the story of the boy Paul Atreides, heir to a noble family tasked with ruling an inhospitable world where the only thing of value is the “spice” melange, a drug capable of extending life and enhancing consciousness. Coveted across the known universe, melange is a prize worth killing for...\r\n\r\nWhen House Atreides is betrayed, the destruction of Paul’s family will set the boy on a journey toward a destiny greater than he could ever have imagined. And as he evolves into the mysterious man known as Muad’Dib, he will bring to fruition humankind’s most ancient and unattainable dream.",
-			"authorId": 53,
-			"foreignBookId": "3634639",
-			"foreignEditionId": "44767458",
-			"titleSlug": "3634639",
+			"overview": "book overview",
+			"authorId": 123,
+			"foreignBookId": "987654",
+			"foreignEditionId": "123123",
+			"titleSlug": "321321",
 			"monitored": true,
 			"anyEditionOk": true,
 			"ratings": {
-				"votes": 784771,
-				"value": 4.33,
-				"popularity": 3398058.43
+				"votes": 123,
+				"value": 4.0,
+				"popularity": 123.0
 			},
-			"releaseDate": "1965-06-01T07:00:00Z",
-			"pageCount": 658,
+			"releaseDate": "1970-01-01T01:00:00Z",
+			"pageCount": 123,
 			"genres": [
-				"Science Fiction",
 				"Fiction",
-				"Fantasy",
 				"Classics",
-				"Audiobook",
-				"Science Fiction Fantasy",
-				"Novels",
-				"Space Opera",
-				"Adventure",
-				"Book Club"
+				"Novels"
 			],
 			"author": {
-				"authorMetadataId": 53,
+				"authorMetadataId": 123,
 				"status": "continuing",
 				"ended": false,
-				"authorName": "Frank Herbert",
-				"authorNameLastFirst": "Herbert, Frank",
-				"foreignAuthorId": "58",
-				"titleSlug": "58",
-				"overview": "Franklin Patrick Herbert Jr. was an American science fiction author best known for the 1965 novel Dune and its five sequels. Though he became famous for his novels, he also wrote short stories and worked as a newspaper journalist, photographer, book reviewer, ecological consultant, and lecturer.The Dune saga, set in the distant future, and taking place over millennia, explores complex themes, such as the long-term survival of the human species, human evolution, planetary science and ecology, and the intersection of religion, politics, economics and power in a future where humanity has long since developed interstellar travel and settled many thousands of worlds. Dune is the best-selling science fiction novel of all time, and the entire series is considered to be among the classics of the genre.",
+				"authorName": "Firstname Lastname",
+				"authorNameLastFirst": "Lastname, Firstname",
+				"foreignAuthorId": "1234",
+				"titleSlug": "1234",
+				"overview": "Firstname Lastname is an author.",
 				"links": [
 					{
-						"url": "https://www.goodreads.com/author/show/58.Frank_Herbert",
-						"name": "Goodreads"
+						"url": "https://www.example.com/author",
+						"name": "Example"
 					}
 				],
 				"images": [
 					{
-						"url": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/authors/1591018335i/58._UX200_CR0,0,200,200_.jpg",
+						"url": "https://www.example.com/author/image",
 						"coverType": "poster",
 						"extension": ".jpg"
 					}
 				],
-				"path": "/books/Frank Herbert",
+				"path": "/books/Firstname Lastname",
 				"qualityProfileId": 1,
 				"metadataProfileId": 2,
 				"monitored": true,
 				"monitorNewItems": "none",
-				"folder": "Frank Herbert",
+				"folder": "Firstname Lastname",
 				"genres": [],
-				"cleanName": "frankherbert",
-				"sortName": "frank herbert",
-				"sortNameLastFirst": "herbert, frank",
+				"cleanName": "firstnamelastname",
+				"sortName": "firstname lastname",
+				"sortNameLastFirst": "lastname, firstname",
 				"tags": [],
-				"added": "2025-11-21T08:58:56Z",
+				"added": "2025-01-01T01:00:00Z",
 				"ratings": {
-					"votes": 1446269,
-					"value": 4.154275,
-					"popularity": 6008199.149975
+					"votes": 123,
+					"value": 4.0,
+					"popularity": 123.0
 				},
 				"statistics": {
 					"bookFileCount": 0,
@@ -94,216 +87,216 @@ var testSearchJSON = `{
 					"sizeOnDisk": 0,
 					"percentOfBooks": 0
 				},
-				"id": 53
+				"id": 123
 			},
 			"images": [
 				{
-					"url": "/MediaCover/Books/187/cover.jpg?lastWrite=636992634250000000",
+					"url": "/MediaCover/Books/123/cover.jpg?lastWrite=636992634250000000",
 					"coverType": "cover",
 					"extension": ".jpg",
-					"remoteUrl": "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1555447414i/44767458.jpg"
+					"remoteUrl": "https://www.example.com/book/cover"
 				}
 			],
 			"links": [
 				{
-					"url": "https://www.goodreads.com/work/3634639-dune",
-					"name": "Goodreads Editions"
+					"url": "https://www.example.com/book/edition",
+					"name": "Example Editions"
 				},
 				{
-					"url": "https://www.goodreads.com/book/show/44767458-dune",
-					"name": "Goodreads Book"
+					"url": "https://www.example.com/book/book",
+					"name": "Example Book"
 				}
 			],
-			"added": "2025-11-21T08:58:56Z",
-			"remoteCover": "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1555447414i/44767458.jpg",
+			"added": "2025-01-01T01:01:00Z",
+			"remoteCover": "https://www.example.com/book/remotecover",
 			"editions": [
 				{
-					"bookId": 187,
-					"foreignEditionId": "44767458",
-					"titleSlug": "44767458",
-					"isbn13": "9780593099322",
-					"asin": "059309932X",
-					"title": "Dune",
+					"bookId": 123,
+					"foreignEditionId": "123",
+					"titleSlug": "123",
+					"isbn13": "123456789012",
+					"asin": "123456789X",
+					"title": "Book",
 					"language": "eng",
-					"overview": "Set on the desert planet Arrakis, Dune is the story of the boy Paul Atreides, heir to a noble family tasked with ruling an inhospitable world where the only thing of value is the “spice” melange, a drug capable of extending life and enhancing consciousness. Coveted across the known universe, melange is a prize worth killing for...\r\n\r\nWhen House Atreides is betrayed, the destruction of Paul’s family will set the boy on a journey toward a destiny greater than he could ever have imagined. And as he evolves into the mysterious man known as Muad’Dib, he will bring to fruition humankind’s most ancient and unattainable dream.",
+					"overview": "book overview",
 					"format": "Hardcover",
 					"isEbook": false,
 					"disambiguation": "",
-					"publisher": "Ace",
-					"pageCount": 658,
-					"releaseDate": "2019-10-01T07:00:00Z",
+					"publisher": "publisher",
+					"pageCount": 123,
+					"releaseDate": "1970-01-01T01:00:00Z",
 					"images": [
 						{
-							"url": "/MediaCover/Books/187/cover.jpg?lastWrite=636992634250000000",
+							"url": "/MediaCover/Books/123/cover.jpg?lastWrite=636992634250000000",
 							"coverType": "cover",
 							"extension": ".jpg",
-							"remoteUrl": "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1555447414i/44767458.jpg"
+							"remoteUrl": "https://example.com/book/edition/image"
 						}
 					],
 					"links": [
 						{
-							"url": "https://www.goodreads.com/book/show/44767458-dune",
-							"name": "Goodreads Book"
+							"url": "https://example.com/book/edition/info",
+							"name": "Example Book"
 						}
 					],
 					"ratings": {
-						"votes": 784771,
-						"value": 4.33,
-						"popularity": 3398058.43
+						"votes": 123,
+						"value": 4.0,
+						"popularity": 123.0
 					},
 					"monitored": true,
 					"manualAdd": false,
 					"grabbed": false,
-					"id": 187
+					"id": 123
 				}
 			],
 			"grabbed": false,
-			"id": 187
+			"id": 123
 		},
 		"id": 2
 }`
 
 var testEditionImage = starr.Image{
+	URL:       "/MediaCover/Books/123/cover.jpg?lastWrite=636992634250000000",
 	CoverType: "cover",
-	URL:       "/MediaCover/Books/187/cover.jpg?lastWrite=636992634250000000",
-	RemoteURL: "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1555447414i/44767458.jpg",
 	Extension: ".jpg",
+	RemoteURL: "https://example.com/book/edition/image",
 }
 
-var testBookImage = testEditionImage
+var testBookImage = starr.Image{
+	URL:       "/MediaCover/Books/123/cover.jpg?lastWrite=636992634250000000",
+	CoverType: "cover",
+	Extension: ".jpg",
+	RemoteURL: "https://www.example.com/book/cover",
+}
 
 var testAuthorImage = starr.Image{
+	URL:       "https://www.example.com/author/image",
 	CoverType: "poster",
-	URL:       "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/authors/1591018335i/58._UX200_CR0,0,200,200_.jpg",
 	Extension: ".jpg",
 }
 
 var testAuthorLink = starr.Link{
-	URL:  "https://www.goodreads.com/author/show/58.Frank_Herbert",
-	Name: "Goodreads",
+	URL:  "https://www.example.com/author",
+	Name: "Example",
 }
 
 var testEditionLink = starr.Link{
-	URL:  "https://www.goodreads.com/book/show/44767458-dune",
-	Name: "Goodreads Book",
+	URL:  "https://example.com/book/edition/info",
+	Name: "Example Book",
 }
 
 var testBookLink1 = starr.Link{
-	URL:  "https://www.goodreads.com/work/3634639-dune",
-	Name: "Goodreads Editions",
+	URL:  "https://www.example.com/book/edition",
+	Name: "Example Editions",
 }
 
-var testBookLink2 = testEditionLink
+var testBookLink2 = starr.Link{
+	URL:  "https://www.example.com/book/book",
+	Name: "Example Book",
+}
 
 var testBookRating = starr.Ratings{
-	Votes:      784771,
-	Value:      4.33,
-	Popularity: 3398058.43,
+	Votes:      123,
+	Value:      4.0,
+	Popularity: 123.0,
 }
 
 var testAuthorRating = starr.Ratings{
-	Votes:      1446269,
-	Value:      4.154275,
-	Popularity: 6008199.149975,
+	Votes:      123,
+	Value:      4.0,
+	Popularity: 123.0,
 }
 
 var testEditionRating = testBookRating
 
 var testEdition = readarr.Edition{
-	ID:               187,
-	BookID:           187,
-	ForeignEditionID: "44767458",
-	TitleSlug:        "44767458",
-	Isbn13:           "9780593099322",
-	Asin:             "059309932X",
-	Title:            "Dune",
-	Overview:         "Set on the desert planet Arrakis, Dune is the story of the boy Paul Atreides, heir to a noble family tasked with ruling an inhospitable world where the only thing of value is the “spice” melange, a drug capable of extending life and enhancing consciousness. Coveted across the known universe, melange is a prize worth killing for...\r\n\r\nWhen House Atreides is betrayed, the destruction of Paul’s family will set the boy on a journey toward a destiny greater than he could ever have imagined. And as he evolves into the mysterious man known as Muad’Dib, he will bring to fruition humankind’s most ancient and unattainable dream.",
+	BookID:           123,
+	ForeignEditionID: "123",
+	TitleSlug:        "123",
+	Isbn13:           "123456789012",
+	Asin:             "123456789X",
+	Title:            "Book",
+	Overview:         "book overview",
 	Format:           "Hardcover",
-	Publisher:        "Ace",
-	PageCount:        658,
-	ReleaseDate:      time.Date(2019, 10, 1, 7, 0, 0, 0, time.UTC),
+	IsEbook:          false,
+	Publisher:        "publisher",
+	PageCount:        123,
+	ReleaseDate:      time.Date(1970, 1, 1, 1, 0, 0, 0, time.UTC),
 	Images:           []*starr.Image{&testEditionImage},
 	Links:            []*starr.Link{&testEditionLink},
 	Ratings:          &testEditionRating,
 	Monitored:        true,
 	ManualAdd:        false,
-	IsEbook:          false,
+	ID:               123,
 }
 
 var testStatistics = readarr.Statistics{
-	BookCount:          0,
 	BookFileCount:      0,
+	BookCount:          0,
+	AvailableBookCount: 0,
 	TotalBookCount:     0,
 	SizeOnDisk:         0,
 	PercentOfBooks:     0,
-	AvailableBookCount: 0,
 }
 
 var testAuthor = readarr.Author{
-	ID:                  53,
+	AuthorMetadataID:    123,
 	Status:              "continuing",
-	AuthorName:          "Frank Herbert",
-	ForeignAuthorID:     "58",
-	TitleSlug:           "58",
-	Overview:            "Franklin Patrick Herbert Jr. was an American science fiction author best known for the 1965 novel Dune and its five sequels. Though he became famous for his novels, he also wrote short stories and worked as a newspaper journalist, photographer, book reviewer, ecological consultant, and lecturer.The Dune saga, set in the distant future, and taking place over millennia, explores complex themes, such as the long-term survival of the human species, human evolution, planetary science and ecology, and the intersection of religion, politics, economics and power in a future where humanity has long since developed interstellar travel and settled many thousands of worlds. Dune is the best-selling science fiction novel of all time, and the entire series is considered to be among the classics of the genre.",
+	Ended:               false,
+	AuthorName:          "Firstname Lastname",
+	AuthorNameLastFirst: "Lastname, Firstname",
+	ForeignAuthorID:     "1234",
+	TitleSlug:           "1234",
+	Overview:            "Firstname Lastname is an author.",
 	Links:               []*starr.Link{&testAuthorLink},
 	Images:              []*starr.Image{&testAuthorImage},
-	Path:                "/books/Frank Herbert",
+	Path:                "/books/Firstname Lastname",
 	QualityProfileID:    1,
 	MetadataProfileID:   2,
+	Monitored:           true,
+	MonitorNewItems:     "none",
 	Genres:              []string{},
-	CleanName:           "frankherbert",
-	SortName:            "frank herbert",
+	CleanName:           "firstnamelastname",
+	SortName:            "firstname lastname",
+	SortNameLastFirst:   "lastname, firstname",
 	Tags:                []int{},
-	Added:               time.Date(2025, 11, 21, 8, 58, 56, 0, time.UTC),
+	Added:               time.Date(2025, 1, 1, 1, 0, 0, 0, time.UTC),
 	Ratings:             &testAuthorRating,
 	Statistics:          &testStatistics,
-	LastBook:            nil,
-	NextBook:            nil,
-	Ended:               false,
-	Monitored:           true,
-	AuthorMetadataID:    53,
-	AuthorNameLastFirst: "Herbert, Frank",
-	MonitorNewItems:     "none",
-	SortNameLastFirst:   "herbert, frank",
+	ID:                  123,
 }
 
 var testSearchStruct = readarr.SearchResult{
-	ForeignID: "3634639",
+	ForeignID: "123456789",
 	ID:        2,
 	Book: &readarr.Book{
-		Added:         time.Date(2025, 11, 21, 8, 58, 56, 0, time.UTC),
-		AnyEditionOk:  true,
-		AuthorID:      53,
-		AuthorTitle:   "herbert, frank Dune",
-		Editions:      []*readarr.Edition{&testEdition},
-		ForeignBookID: "3634639",
+		Title:          "Book",
+		AuthorTitle:    "lastname, firstname Book",
+		SeriesTitle:    "",
+		Disambiguation: "",
+		Overview:       "book overview",
+		AuthorID:       123,
+		ForeignBookID:  "987654",
+		TitleSlug:      "321321",
+		Monitored:      true,
+		AnyEditionOk:   true,
+		Ratings:        &testBookRating,
+		ReleaseDate:    time.Date(1970, 1, 1, 1, 0, 0, 0, time.UTC),
+		PageCount:      123,
 		Genres: []string{
-			"Science Fiction",
 			"Fiction",
-			"Fantasy",
 			"Classics",
-			"Audiobook",
-			"Science Fiction Fantasy",
 			"Novels",
-			"Space Opera",
-			"Adventure",
-			"Book Club",
 		},
-		ID:          187,
+		Author:      &testAuthor,
 		Images:      []*starr.Image{&testBookImage},
 		Links:       []*starr.Link{&testBookLink1, &testBookLink2},
-		Monitored:   true,
+		Added:       time.Date(2025, 1, 1, 1, 1, 0, 0, time.UTC),
+		RemoteCover: "https://www.example.com/book/remotecover",
+		Editions:    []*readarr.Edition{&testEdition},
 		Grabbed:     false,
-		Overview:    "Set on the desert planet Arrakis, Dune is the story of the boy Paul Atreides, heir to a noble family tasked with ruling an inhospitable world where the only thing of value is the “spice” melange, a drug capable of extending life and enhancing consciousness. Coveted across the known universe, melange is a prize worth killing for...\r\n\r\nWhen House Atreides is betrayed, the destruction of Paul’s family will set the boy on a journey toward a destiny greater than he could ever have imagined. And as he evolves into the mysterious man known as Muad’Dib, he will bring to fruition humankind’s most ancient and unattainable dream.",
-		PageCount:   658,
-		Ratings:     &testBookRating,
-		RemoteCover: "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1555447414i/44767458.jpg",
-		ReleaseDate: time.Date(1965, 6, 1, 7, 0, 0, 0, time.UTC),
-		SeriesTitle: "",
-		Title:       "Dune",
-		TitleSlug:   "3634639",
-		Author:      &testAuthor,
+		ID:          123,
 	},
 	Author: nil,
 }
@@ -314,10 +307,10 @@ func TestSearch(t *testing.T) {
 	tests := []*starrtest.MockData{
 		{
 			Name:           "200",
-			ExpectedPath:   path.Join("/", starr.API, readarr.APIver, "search?term=Dune"),
+			ExpectedPath:   path.Join("/", starr.API, readarr.APIver, "search?term=book"),
 			ResponseStatus: http.StatusOK,
 			ResponseBody:   `[` + testSearchJSON + `]`,
-			WithRequest:    "Dune",
+			WithRequest:    "book",
 			WithError:      nil,
 			ExpectedMethod: http.MethodGet,
 			WithResponse:   []*readarr.SearchResult{&testSearchStruct},
